9546129626d9d56d71ee37a7ae2da0e9755a188f
Toleu Bahauddin <baha200477@gmail.com>
2024-06-08 16:48:49 +0500

    deploy front

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot([{path:'',component:HomeComponent}])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
